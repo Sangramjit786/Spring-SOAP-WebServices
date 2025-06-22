8f6e48779044772c54c55d5d7f92a1ea312b7277
Rohit <nathrohit48@gmail.com>
2025-06-22 10:02:35 +0530

    Created service to implement to SOAP API.

---
+++ src/main/java/com/soap/webservices/SpringSoapWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.soap.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapWebServicesApplication.class, args);
+	}
+
+}
